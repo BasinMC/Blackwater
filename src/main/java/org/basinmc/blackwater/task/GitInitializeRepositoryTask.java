9cffb3d374e0e1ee2626a70f8f067a903b69b715
Johannes Donath <johannesd@torchmind.com>
2018-02-20 10:22:08 +0100

    Made the initialization task configurable.

--- src/main/java/org/basinmc/blackwater/task/GitInitializeRepositoryTask.java
+++ src/main/java/org/basinmc/blackwater/task/GitInitializeRepositoryTask.java
@@ -4,6 +4,9 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
+import org.basinmc.blackwater.artifact.ArtifactManager;
+import org.basinmc.blackwater.artifact.ArtifactReference;
 import org.basinmc.blackwater.task.error.TaskExecutionException;
 import org.eclipse.jgit.api.InitCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -15,13 +18,22 @@ import org.slf4j.LoggerFactory;
  *
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
-public class GitInitializeRepositoryTask implements Task {
+public class GitInitializeRepositoryTask extends AbstractConfigurableTask {
 
   private static final Logger logger = LoggerFactory.getLogger(GitInitializeRepositoryTask.class);
 
   private final Path path;
 
   public GitInitializeRepositoryTask(@NonNull Path path) {
+    this.path = path;
+  }
+
+  private GitInitializeRepositoryTask(
+      @NonNull Path path,
+      @NonNull Set<Class<? extends Task>> optionalTasks,
+      @NonNull Set<ArtifactReference> requiredArtifacts,
+      @NonNull Set<Class<? extends Task>> requiredTasks) {
+    super(optionalTasks, requiredArtifacts, requiredTasks);
     this.path = path;
   }
 
@@ -55,6 +67,86 @@ public class GitInitializeRepositoryTask implements Task {
           .call();
     } catch (GitAPIException | IOException ex) {
       throw new TaskExecutionException("Failed to initialize repository: " + ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * Provides a factory for the git initialization task.
+   */
+  public static class Builder extends
+      AbstractConfigurableTask.Builder<GitInitializeRepositoryTask> {
+
+    private Path path;
+
+    /**
+     * Constructs a new git initialization task.
+     *
+     * @return a new task instance.
+     * @throws IllegalStateException when no path has been configured.
+     */
+    @NonNull
+    public GitInitializeRepositoryTask build() {
+      if (this.path == null) {
+        throw new IllegalStateException("Illegal task configuration: Directory path is required");
+      }
+
+      return new GitInitializeRepositoryTask(
+          this.path,
+          this.optionalTasks,
+          this.requiredArtifacts,
+          this.requiredTasks
+      );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @NonNull
+    @Override
+    public GitInitializeRepositoryTask build(@NonNull ArtifactManager manager) {
+      return this.build();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @NonNull
+    @Override
+    public Builder withOptionalTask(@NonNull Class<? extends Task> task) {
+      super.withOptionalTask(task);
+      return this;
+    }
+
+    /**
+     * Specifies a path to initialize the repository in.
+     *
+     * @param path a path pointing to a directory in which the repository should be initialized.
+     * @return a reference to this builder.
+     */
+    @NonNull
+    public Builder withPath(@NonNull Path path) {
+      this.path = path;
+      return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @NonNull
+    @Override
+    public Builder withRequiredArtifact(@NonNull ArtifactReference reference) {
+      super.withRequiredArtifact(reference);
+      return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @NonNull
+    @Override
+    public Builder withRequiredTask(@NonNull Class<? extends Task> task) {
+      super.withRequiredTask(task);
+      return this;
     }
   }
 }
