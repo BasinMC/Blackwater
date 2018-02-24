a2729bc9ba260c82025f94b5ea265651167a2cab
Johannes Donath <johannesd@torchmind.com>
2018-02-24 12:35:21 +0100

    Added support for skipping the task when a repository already exists.

--- tasks-git/src/main/java/org/basinmc/blackwater/tasks/git/GitInitTask.java
+++ tasks-git/src/main/java/org/basinmc/blackwater/tasks/git/GitInitTask.java
@@ -2,12 +2,15 @@ package org.basinmc.blackwater.tasks.git;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import org.basinmc.blackwater.task.Task;
 import org.basinmc.blackwater.task.error.TaskExecutionException;
 import org.basinmc.blackwater.task.error.TaskParameterException;
 import org.eclipse.jgit.api.InitCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Initializes a new git repository at the specified target path.
@@ -15,6 +18,8 @@ import org.eclipse.jgit.api.errors.GitAPIException;
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
 public class GitInitTask implements Task {
+
+  private static final Logger logger = LoggerFactory.getLogger(GitInitTask.class);
 
   /**
    * {@inheritDoc}
@@ -28,7 +33,13 @@ public class GitInitTask implements Task {
       throw new TaskParameterException("Output path cannot be on a custom filesystem");
     }
 
+    if (Files.exists(outputPath.resolve(".git"))) {
+      logger.info("Repository exists - Skipped execution");
+      return;
+    }
+
     try {
+      logger.info("Creating a new empty git repository");
       new InitCommand()
           .setDirectory(outputPath.toFile())
           .call();
