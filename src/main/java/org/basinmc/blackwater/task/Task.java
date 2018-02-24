7a8b4b41e44378bda99bd7c0e5e3f9b87b2d97b8
Johannes Donath <johannesd@torchmind.com>
2018-02-24 02:20:31 +0100

    Added a parameter builder interface to facilitate the base configuration of contextual task parameters and execution parameters.

--- src/main/java/org/basinmc/blackwater/task/Task.java
+++ src/main/java/org/basinmc/blackwater/task/Task.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 import javax.annotation.Nonnull;
+import org.basinmc.blackwater.Pipeline;
 import org.basinmc.blackwater.artifact.Artifact;
+import org.basinmc.blackwater.artifact.ArtifactReference;
 import org.basinmc.blackwater.task.error.TaskExecutionException;
 import org.basinmc.blackwater.task.error.TaskParameterException;
 
@@ -94,5 +96,77 @@ public interface Task {
      */
     @Nonnull
     Optional<Path> getOutputPath();
+  }
+
+  /**
+   * Provides a factory for contextual task parameters (such as input and output tasks) as well as
+   * some other execution related parameters.
+   */
+  interface ParameterBuilder {
+
+    /**
+     * Assembles the parameters within this builder and adds the task to the pipeline at its
+     * designated position.
+     */
+    @Nonnull
+    Pipeline.Builder build();
+
+    /**
+     * Selects an artifact as an input directory for the task.
+     *
+     * @param artifact an artifact reference.
+     * @return a reference to this builder.
+     */
+    @Nonnull
+    ParameterBuilder withInputArtifact(@Nonnull ArtifactReference artifact);
+
+    /**
+     * Selects a file or directory as an input for the task.
+     *
+     * @param file an input file or directory.
+     * @return a reference to this builder.
+     */
+    @Nonnull
+    ParameterBuilder withInputFile(@Nonnull Path file);
+
+    /**
+     * <p>Selects an artifact as an output directory for the task.</p>
+     *
+     * <p>Unless {@link #withForcedExecution(boolean)} is specified, the task will evaluate whether
+     * the specified output artifact exists and is valid. If so, the task will not be executed in
+     * order to permit following tasks to rely on the cached version of the task output.</p>
+     *
+     * @param artifact an artifact reference.
+     * @return a reference to this builder.
+     */
+    @Nonnull
+    ParameterBuilder withOutputArtifact(@Nonnull ArtifactReference artifact);
+
+    /**
+     * Selects a directory as an output for the task.
+     *
+     * @param file an output directory (or path in a custom {@link java.nio.file.FileSystem}).
+     * @return a reference to this builder.
+     */
+    @Nonnull
+    ParameterBuilder withOutputFile(@Nonnull Path file);
+
+    /**
+     * @see #withForcedExecution(boolean)
+     */
+    @Nonnull
+    default ParameterBuilder withForcedExecution() {
+      return this.withForcedExecution(true);
+    }
+
+    /**
+     * Selects whether or not the pipeline is permitted to rely on cached artifacts when they are
+     * deemed valid or whether execution of the task is always expected.
+     *
+     * @param value if true executes the task at all times, otherwise evaluates cached artifacts.
+     * @return a reference to this builder.
+     */
+    @Nonnull
+    ParameterBuilder withForcedExecution(boolean value);
   }
 }
