133d7bef51418417e7b6f11ac5595ad7ec4aca11
Johannes Donath <johannesd@torchmind.com>
2018-02-24 14:56:41 +0100

    Created a task to apply an arbitrary set of git patches.

---
+++ tasks-git/src/main/java/org/basinmc/blackwater/tasks/git/GitApplyMailArchiveTask.java
@@ -0,0 +1,129 @@
+package org.basinmc.blackwater.tasks.git;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+import javax.annotation.Nullable;
+import org.basinmc.blackwater.task.error.TaskExecutionException;
+import org.basinmc.blackwater.task.error.TaskParameterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class GitApplyMailArchiveTask extends AbstractExecutableTask {
+
+  private static final Logger logger = LoggerFactory.getLogger(GitApplyMailArchiveTask.class);
+
+  private static final Pattern PATCH_FILE_FORMAT = Pattern.compile("^(\\d{4,})-([\\w.-]+)$");
+
+  private final String referenceBranch;
+
+  public GitApplyMailArchiveTask(@Nullable String referenceBranch) {
+    this.referenceBranch = referenceBranch;
+  }
+
+  public GitApplyMailArchiveTask() {
+    this(null);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void execute(@NonNull Context context) throws TaskExecutionException {
+    Path inputPath = context.getInputPath()
+        .orElseThrow(() -> new TaskParameterException("Input path is required"));
+
+    if (inputPath.getFileSystem() != FileSystems.getDefault()) {
+      throw new TaskParameterException("Input path cannot be on a custom filesystem");
+    }
+
+    Path outputPath = context.getOutputPath()
+        .orElseThrow(() -> new TaskParameterException("Output path is required"));
+
+    if (Files.notExists(outputPath.resolve(".git"))) {
+      throw new TaskExecutionException("No repository at output path");
+    }
+
+    if (outputPath.getFileSystem() != FileSystems.getDefault()) {
+      throw new TaskParameterException("Output path cannot be on a custom filesystem");
+    }
+
+    // before we're trying to apply the mail archive, we'll make sure that no merge is still in
+    // progress (either regular or mail archive)
+    logger.info("Aborting any pending merge processes ...");
+    if (this.execute(new ProcessBuilder("git", "am", "--abort")
+        .directory(outputPath.toFile())) != 0) {
+      logger.info("  No mail archive merge in progress");
+    } else {
+      logger.warn("  Mail archive merge aborted");
+    }
+
+    if (this.execute(new ProcessBuilder("git", "merge", "--abort")
+        .directory(outputPath.toFile())) != 0) {
+      logger.info("  No merge in progress");
+    } else {
+      logger.warn("  Merge aborted");
+    }
+
+    // when we were given a reference branch, we'll revert back to it before applying our patches as
+    // it acts as an indication that the source code will actually be mostly defined by the patches
+    if (this.referenceBranch != null) {
+      logger.info("Reverting to reference branch");
+
+      if (this.execute(new ProcessBuilder("git", "reset", "--hard", this.referenceBranch)
+          .directory(outputPath.toFile())) != 0) {
+        throw new TaskExecutionException(
+            "Failed to revert back to reference branch: Git command returned an error");
+      }
+
+      logger.info("  Success");
+    }
+
+    // since the repository is now in an acceptable state, we'll iterate over the patches in our
+    // input directory one by one in their respective order in their originating history (e.g. based
+    // on their prefix)
+    try {
+      Iterator<Path> it = Files.walk(inputPath, 1)
+          .filter((p) -> PATCH_FILE_FORMAT.matcher(p.getFileName().toString()).matches())
+          .sorted((p1, p2) -> {
+            int p1Index = Integer
+                .parseUnsignedInt(PATCH_FILE_FORMAT.matcher(p1.getFileName().toString()).group(1));
+            int p2Index = Integer
+                .parseUnsignedInt(PATCH_FILE_FORMAT.matcher(p2.getFileName().toString()).group(1));
+
+            return Integer.compareUnsigned(p1Index, p2Index);
+          })
+          .iterator();
+
+      while (it.hasNext()) {
+        Path patch = outputPath.relativize(it.next());
+        logger.info("  Applying {}", patch.getFileName());
+
+        if (this.execute(
+            new ProcessBuilder("git", "am", "--ignore-whitespace", "--3way", patch.toString())
+                .directory(outputPath.toFile())) != 0) {
+          logger.info("    Failed");
+          throw new TaskExecutionException("Failed to apply patch: Git command returned an error");
+        }
+      }
+    } catch (IOException ex) {
+      throw new TaskExecutionException("Failed to access patch files: " + ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @NonNull
+  @Override
+  public String getName() {
+    return "git-am";
+  }
+}
---
+++ tasks-git/src/test/java/org/basinmc/blackwater/tasks/git/GitApplyMailArchiveTaskTest.java
@@ -0,0 +1,60 @@
+package org.basinmc.blackwater.tasks.git;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import org.basinmc.blackwater.task.Task.Context;
+import org.basinmc.blackwater.task.error.TaskExecutionException;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.InitCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class GitApplyMailArchiveTaskTest extends AbstractExecutableGitTaskTest {
+
+  @Test
+  public void testExecute() throws IOException, GitAPIException, TaskExecutionException {
+    this.extract("/am-apply/test", Paths.get("test"));
+    this.extract("/am-apply/0001-Test-Patch.patch", Paths.get("patches/0001-Test-Patch.patch"));
+
+    InitCommand command = Git.init()
+        .setDirectory(this.getBase().toFile());
+
+    try (Git git = command.call()) {
+      // re-create the expected repository state manually here to create a realistic test
+      // environment
+      git.add()
+          .addFilepattern("test")
+          .call();
+      git.commit()
+          .setAuthor("John Doe", "john@example.org")
+          .setCommitter("John Doe", "john@example.org")
+          .setMessage("Initial Commit")
+          .call();
+
+      // now we can simply mock the context and run the task to produce the end result and compare
+      // it against our target values
+      Context context = Mockito.mock(Context.class);
+      Mockito.when(context.getInputPath())
+          .thenReturn(Optional.of(this.getBase().resolve("patches")));
+      Mockito.when(context.getOutputPath())
+          .thenReturn(Optional.of(this.getBase()));
+
+      GitApplyMailArchiveTask task = new GitApplyMailArchiveTask();
+      task.execute(context);
+
+      String expected = "This is a patched test file.\n";
+      String actual = new String(Files.readAllBytes(this.getBase().resolve("test")),
+          StandardCharsets.UTF_8);
+
+      Assert.assertEquals(expected, actual);
+    }
+  }
+}
