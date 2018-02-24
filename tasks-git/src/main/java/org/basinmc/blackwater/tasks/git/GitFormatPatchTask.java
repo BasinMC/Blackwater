50b60b298a9a505c05adb7013ac9bcec8ee95792
Johannes Donath <johannesd@torchmind.com>
2018-02-24 16:21:10 +0100

    Created a git format-patch task.

---
+++ tasks-git/src/main/java/org/basinmc/blackwater/tasks/git/GitFormatPatchTask.java
@@ -0,0 +1,94 @@
+package org.basinmc.blackwater.tasks.git;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.annotation.Nonnull;
+import org.basinmc.blackwater.task.error.TaskExecutionException;
+import org.basinmc.blackwater.task.error.TaskParameterException;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Generates a mail archive of all changes between the current and a reference branch.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class GitFormatPatchTask extends AbstractExecutableTask {
+
+  private static final Logger logger = LoggerFactory.getLogger(GitFormatPatchTask.class);
+
+  private final String referenceBranch;
+
+  public GitFormatPatchTask(@Nonnull String referenceBranch) {
+    this.referenceBranch = referenceBranch;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void execute(@NonNull Context context) throws TaskExecutionException {
+    Path inputPath = context.getInputPath()
+        .orElseThrow(() -> new TaskParameterException("Input path is required"));
+
+    if (inputPath.getFileSystem() != FileSystems.getDefault()) {
+      throw new TaskParameterException("Input path cannot be on a custom filesystem");
+    }
+
+    if (Files.notExists(inputPath.resolve(".git"))) {
+      throw new TaskExecutionException("No repository at output path");
+    }
+
+    Path outputPath = context.getOutputPath()
+        .orElseThrow(() -> new TaskParameterException("Output path is required"));
+
+    if (outputPath.getFileSystem() != FileSystems.getDefault()) {
+      throw new TaskParameterException("Output path cannot be on a custom filesystem");
+    }
+
+    // check whether the repository is in an unclean state first to give users some basic feedback
+    // on whether all their changes are included
+    FileRepositoryBuilder builder = new FileRepositoryBuilder()
+        .setWorkTree(inputPath.toFile());
+
+    try (Repository repository = builder.build()) {
+      try (Git git = new Git(repository)) {
+        if (!git.status().call().isClean()) {
+          logger.warn("Repository is not in a clean state");
+          logger.warn("Unstaged changes will be omitted");
+        }
+      }
+    } catch (GitAPIException | IOException ex) {
+      throw new TaskExecutionException("Failed to access repository: " + ex.getMessage(), ex);
+    }
+
+    // next up, we'll have to invoke the git executable again (since jgit does not provide this
+    // functionality)
+    logger.info("Generating patches against reference branch \"{}\"", this.referenceBranch);
+    Path path = inputPath.relativize(outputPath);
+
+    if (this.execute(
+        new ProcessBuilder("git", "format-patch", "-p", "--minimal", "-N", "-o", path.toString(),
+            this.referenceBranch).directory(inputPath.toFile())) != 0) {
+      throw new TaskExecutionException("Failed to generate patches: Git command returned an error");
+    }
+
+    logger.info("  Success");
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @NonNull
+  @Override
+  public String getName() {
+    return "git-format-patch";
+  }
+}
---
+++ tasks-git/src/test/java/org/basinmc/blackwater/tasks/git/GitFormatPatchTaskTest.java
@@ -0,0 +1,71 @@
+package org.basinmc.blackwater.tasks.git;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import org.basinmc.blackwater.task.Task;
+import org.basinmc.blackwater.task.Task.Context;
+import org.basinmc.blackwater.task.error.TaskExecutionException;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Evaluates whether {@link GitFormatPatchTask} operates as expected.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class GitFormatPatchTaskTest extends AbstractExecutableGitTaskTest {
+
+  /**
+   * Evaluates whether the task executes as expected.
+   */
+  @Test
+  public void testExecute() throws IOException, GitAPIException, TaskExecutionException {
+    try (Git git = Git.init().setDirectory(this.getBase().toFile()).call()) {
+      Files.write(this.getBase().resolve("test"),
+          "This is a test file".getBytes(StandardCharsets.UTF_8));
+
+      git.add()
+          .addFilepattern("test")
+          .call();
+      git.commit()
+          .setCommitter("John Doe", "john@example.org")
+          .setMessage("Initial Commit")
+          .call();
+      git.branchCreate()
+          .setName("upstream")
+          .call();
+
+      Files.write(this.getBase().resolve("test"),
+          "This is a patched test file".getBytes(StandardCharsets.UTF_8));
+
+      git.add()
+          .addFilepattern("test")
+          .call();
+      git.commit()
+          .setCommitter("John Doe", "john@example.org")
+          .setMessage("Update Commit")
+          .call();
+    }
+
+    Path patchDirectory = this.getBase().resolve("patches");
+
+    Context ctx = Mockito.mock(Context.class);
+    Mockito.when(ctx.getInputPath())
+        .thenReturn(Optional.of(this.getBase()));
+    Mockito.when(ctx.getOutputPath())
+        .thenReturn(Optional.of(patchDirectory));
+
+    Task task = new GitFormatPatchTask("upstream");
+    task.execute(ctx);
+
+    Path patchPath = patchDirectory.resolve("0001-Update-Commit.patch");
+    Assert.assertTrue(Files.exists(patchPath));
+    Assert.assertTrue(Files.isRegularFile(patchPath));
+  }
+}
