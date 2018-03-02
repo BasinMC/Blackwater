1150c4e13d8d5dceb105efd8fc687ca4e77e90ea
Johannes Donath <johannesd@torchmind.com>
2018-03-02 15:15:32 +0100

    Renamed the fetch artifact task to be more accurate in its result.

---
+++ tasks-maven/src/main/java/org/basinmc/blackwater/tasks/maven/ResolveArtifactTask.java
@@ -0,0 +1,59 @@
+package org.basinmc.blackwater.tasks.maven;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import javax.annotation.Nonnull;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.shared.dependencies.DependableCoordinate;
+import org.apache.maven.shared.dependencies.resolve.DependencyResolver;
+import org.apache.maven.shared.dependencies.resolve.DependencyResolverException;
+import org.basinmc.blackwater.task.Task;
+import org.basinmc.blackwater.task.error.TaskExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Retrieves, resolves and installs an artifact (and all of its dependencies) to the local maven
+ * repository.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class ResolveArtifactTask implements Task {
+
+  private static final Logger logger = LoggerFactory.getLogger(ResolveArtifactTask.class);
+
+  private final DependencyResolver resolver;
+  private final ProjectBuildingRequest buildingRequest;
+  private final DependableCoordinate coordinate;
+
+  public ResolveArtifactTask(
+      @Nonnull DependencyResolver resolver,
+      @Nonnull ProjectBuildingRequest buildingRequest,
+      @NonNull DependableCoordinate coordinate) {
+    this.resolver = resolver;
+    this.buildingRequest = buildingRequest;
+    this.coordinate = coordinate;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void execute(@NonNull Context context) throws TaskExecutionException {
+    try {
+      logger.info("Fetching artifact {} and all of its dependencies ...", this.coordinate);
+      this.resolver.resolveDependencies(this.buildingRequest, this.coordinate, null);
+    } catch (DependencyResolverException ex) {
+      throw new TaskExecutionException(
+          "Failed to resolve artifact " + this.coordinate + ": " + ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @NonNull
+  @Override
+  public String getName() {
+    return "maven-resolve-artifact";
+  }
+}
